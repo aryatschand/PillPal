ffbe1c87a49a76632c6f64a3a97ac9e266aab2f5
Arya Tschand <arya@voicesaver.com>
2019-12-01 10:07:20 -0500

    Create XCode File

---
+++ iOS App/QBHacksApp/QBHacksApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QBHacksApp
+//
+//  Created by Arya Tschand on 12/1/19.
+//  Copyright © 2019 Arya Tschand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
